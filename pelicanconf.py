046ac30275fd5862a43e904150cdc701957bc0f5
Prasanna Pandit <prasanna.pandit@gmail.com>
2015-11-20 19:33:52 +0530

    Changed theme to blue-pelican. Changes to conf files. README updated to reflect make html for local preview/

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,9 +29,42 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 DEFAULT_PAGINATION = 10
 
-THEME = '/home/pvpandit/blog/pelican-themes/elegant'
+THEME = '/home/pvpandit/blog/pelican-themes/blue-penguin'
 
 LANDING_PAGE_ABOUT = {'title': 'Prasanna Pandit', 'details': 'Occasional blogger. Federer fan. Love non-fiction and lolz. Went to IISc and drank lots of tea.'};
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+#Settings for blue-pelican theme
+
+# all the following settings are *optional*
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    #('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+#MENUITEMS = (
+ #   ('GitHub', 'https://github.com/'),
+  #  ('Linux Kernel', 'https://www.kernel.org/'),
+#)
